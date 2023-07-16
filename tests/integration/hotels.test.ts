13cac07018a4f0c501ead8dea4abcc09a20eb644
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-07-16 12:14:37 -0300

    feat: first success test case in GET /hotels

--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -1,6 +1,15 @@
 import supertest from 'supertest';
+import * as jwt from 'jsonwebtoken';
 import httpStatus from 'http-status';
-import { cleanDb } from '../helpers';
+import faker from '@faker-js/faker';
+import {
+  createEnrollmentWithAddress,
+  createSession,
+  createTicket,
+  createTicketTypeInPersonWithHotel,
+  createUser,
+} from '../factories';
+import { cleanDb, generateValidToken } from '../helpers';
 import app, { init } from '@/app';
 
 beforeAll(async () => {
@@ -18,5 +27,48 @@ describe('GET /hotels', () => {
     const result = await server.get('/hotels');
 
     expect(result.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word;
+
+    const result = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+
+    expect(result.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session fot the given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const result = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+
+    expect(result.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+});
+
+describe('GET /hotels when token is valid', () => {
+  it('should return array of hotels and status code 200 if meets all the business rules', async () => {
+    const user = await createUser();
+    const token = await generateValidToken(user); //usar quando fizer requisição
+    const session = await createSession(token);
+    const enrollment = await createEnrollmentWithAddress(user);
+    const ticketType = await createTicketTypeInPersonWithHotel();
+    const paidTicket = await createTicket(enrollment.id, ticketType.id, 'PAID');
+
+    const result = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+
+    expect(result.status).toBe(200);
+    expect(result.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          image: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        }),
+      ]),
+    );
   });
 });
